d955ab65c1243dcd09311b4140970c1c6c656945
Julia Stenina <Julia.Hono@yandex.ru>
2023-05-11 22:10:29 +0300

    Внесла правки согласно комментариям проверяющего в тестовые классы

--- src/test/java/TestsForClassCat.java
+++ src/test/java/TestsForClassCat.java
@@ -4,14 +4,14 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestsForClassCat {
 
-    @Spy
+    @Mock
     public Feline feline;
 
     @Test
@@ -26,6 +26,7 @@ public class TestsForClassCat {
     @Test
     public void testGetFood() throws Exception {
         Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
 
         Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
     }
--- src/test/java/TestsForClassLion.java
+++ src/test/java/TestsForClassLion.java
@@ -4,14 +4,14 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestsForClassLion {
 
-    @Spy
+    @Mock
     Feline feline;
 
     @Test(expected = Exception.class)
@@ -22,6 +22,7 @@ public class TestsForClassLion {
     @Test
     public void testGetKittens() throws Exception {
         Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
 
         Assert.assertEquals(1, lion.getKittens());
     }
@@ -38,6 +39,7 @@ public class TestsForClassLion {
     @Test
     public void testGetFood() throws Exception {
         Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
 
         Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
     }
